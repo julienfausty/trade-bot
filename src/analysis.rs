e67af28f664fb12705019b1166cce5426c8954b7
julienfausty <julienfausty@gmail.com>
2025-11-10 14:02:18 +0100

    test: add unittesting to analysis module

--- src/analysis.rs
+++ src/analysis.rs
@@ -6,7 +6,7 @@ use std::{
     sync::RwLock,
 };
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct OHLC {
     pub time: i64,
     pub open: f32,
@@ -100,7 +100,7 @@ impl MovingStatistics {
                 Some(pair) => {
                     popped = Some(pair.1);
                 }
-                None => (),
+                None => return Ok(None),
             }
         }
 
@@ -136,9 +136,9 @@ impl MovingStatistics {
 
         let mut prefix_sum: Vec<OHLC> = read_universe
             .iter()
-            .map(|element| {
+            .map(|(_, element)| {
                 let cloned = sum_buffer.clone();
-                sum_buffer += element.1.clone();
+                sum_buffer += element.clone();
                 return cloned;
             })
             .collect();
@@ -148,18 +148,140 @@ impl MovingStatistics {
         let mut means = HashMap::new();
 
         for window in &windows {
-            let number_shifts = self.universe_window - *window;
+            let number_shifts = self.universe_window - (*window - 1);
 
             let moving_average: Vec<OHLC> = (0..number_shifts)
                 .map(|i_shift| {
-                    (prefix_sum[i_shift].clone() - prefix_sum[i_shift - window].clone()) / *window
+                    (prefix_sum[i_shift + *window].clone() - prefix_sum[i_shift].clone()) / *window
                 })
-                .rev()
                 .collect();
 
             means.insert(window.clone(), moving_average);
         }
 
         Ok(means)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn zero_ohlc() -> OHLC {
+        OHLC {
+            time: 0,
+            open: 0.0,
+            high: 0.0,
+            low: 0.0,
+            close: 0.0,
+            vwap: 0.0,
+            volume: 0.0,
+            count: 0,
+        }
+    }
+
+    fn const_ohlc(val: f32) -> OHLC {
+        OHLC {
+            time: val as i64,
+            open: val,
+            high: val,
+            low: val,
+            close: val,
+            vwap: val,
+            volume: val,
+            count: val as i64,
+        }
+    }
+
+    async fn analytical_case(universe: usize) -> MovingStatistics {
+        let mut stats = MovingStatistics::new(universe);
+        for i_universe in 0..universe {
+            let _ = stats.update(const_ohlc(i_universe as f32)).await;
+        }
+        stats
+    }
+
+    #[tokio::test]
+    async fn test_vanilla_update() {
+        let mut stats = MovingStatistics::new(10);
+
+        assert!(stats.update(zero_ohlc()).await == Ok(None));
+        assert!(stats.update(zero_ohlc()).await == Ok(None));
+    }
+
+    #[tokio::test]
+    async fn test_null_universe_update() {
+        let mut stats = MovingStatistics::new(0);
+
+        let updated = stats.update(zero_ohlc()).await;
+        assert!(updated.is_ok());
+        assert!(updated == Ok(None));
+    }
+
+    #[tokio::test]
+    async fn test_one_universe_update() {
+        let mut stats = MovingStatistics::new(1);
+
+        let updated = stats.update(zero_ohlc()).await;
+        assert!(updated.is_ok());
+        assert!(updated == Ok(None));
+
+        let updated = stats.update(zero_ohlc()).await;
+        assert!(updated == Ok(Some(zero_ohlc())));
+    }
+
+    #[tokio::test]
+    async fn test_analytical_analysis() {
+        let universe = 100;
+        let stats = analytical_case(universe).await;
+
+        let means_result = stats.means(vec![1, 2, 5, 10, 100]).await;
+        assert!(means_result.is_ok());
+        let means = means_result.unwrap();
+
+        assert!(means.len() == 5);
+
+        assert!(means.contains_key(&1));
+        assert!(
+            means[&1]
+                == (0..universe)
+                    .map(|val| const_ohlc(val as f32))
+                    .collect::<Vec<_>>()
+        );
+
+        assert!(means.contains_key(&2));
+        assert!(
+            means[&2]
+                == (0..(universe - 1))
+                    .map(|val| (const_ohlc(val as f32) + const_ohlc((val + 1) as f32)) / 2)
+                    .collect::<Vec<_>>()
+        );
+
+        assert!(means.contains_key(&5));
+        assert!(
+            means[&5]
+                == (2..(universe - 2))
+                    .map(|val| const_ohlc(val as f32))
+                    .collect::<Vec<_>>()
+        );
+
+        assert!(means.contains_key(&10));
+        assert!(
+            means[&10]
+                == (4..(universe - 5))
+                    .map(|val| (const_ohlc(val as f32) + const_ohlc((val + 1) as f32)) / 2)
+                    .collect::<Vec<_>>()
+        );
+
+        assert!(means.contains_key(&100));
+        assert!(means[&100] == vec![const_ohlc(49.5)]);
+    }
+
+    #[tokio::test]
+    async fn test_window_larger_than_universe() {
+        let stats = analytical_case(10).await;
+
+        assert!(stats.means(vec![11]).await.is_err());
     }
 }
